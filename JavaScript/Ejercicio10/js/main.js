acfd63b526dda7c4da2a3d8741451228c7ff4857
Iliana-Maldonado <nana.malibily@gmail.com>
2020-05-29 17:33:17 -0500

    m

---
+++ JavaScript/Ejercicio10/js/main.js
@@ -0,0 +1,8 @@
+// Preguntar al usuario el nombre de una figura geométrica (Cuadrado, Rectángulo, Circulo, Triangulo)//
+//Preguntar al usuario que desea calcular (area o perimetro)//
+//Dependiendo de la figura y lo que se desea calcular, aplcar la formula correspondiente y mostrar al usuario en un alert el resultado del calculo//
+
+
+//Pedirle al usuario un numero entre 0 y 50//
+//Si el numero ingresado por el usuario sale de rango, pedirle que ingrese un numero diferente//
+//Si el numero es mayor a 0, imprimir en consola los numeros pares que existan entre 0 y el numero ingresado por el calculo//
