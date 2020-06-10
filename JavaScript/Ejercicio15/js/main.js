e6bc4f40c4bd31b97c05756cc5b227c1841be8b0
Iliana-Maldonado <nana.malibily@gmail.com>
2020-06-09 22:42:33 -0500

    :pencil2: Extraccion de DOM

--- JavaScript/Ejercicio15/js/main.js
+++ JavaScript/Ejercicio15/js/main.js
@@ -52,7 +52,7 @@ var newArray = songsArray.reduce( ( accum, song ) => {
 },[])*/
 
 /*var nameInput = document.getElementById('user-name');
-console.log(nameInput.value) //Entrega un documento
+console.log(nameInput.value) //Entrega un solo elemento
 var inputs = document.getElementsByClassName('form-control');
 console.log(inputs) //Entrega un Elemento
 var inputsByName = document.getElementsByName('user')
@@ -72,54 +72,42 @@ Extraer datos del formulario.
 -métodos de DOM (getElementById, ClassName, Name, etc)
 diferencias entre Array, Object, HTMLCollection, NodeLists*/
 
-var songsArray = [
-    {
-        name: "Thunderstruck",
-        interpreter: "AC / DC",
-        gender: "Rock"
-    }, {
-        name: "Fear of the dark",
-        interpreter: "Iron Maiden",
-        gender: "Metal"
-    }, {
-        name: "Whole lotta Rossie",
-        interpreter: "AC / DC",
-        gender: "Rock"
-    }, {
-        name: "Electric Eye",
-        interpreter: "Judas Priest",
-        gender: "Metal"
-    }, {
-        name: "The show must go on",
-        interpreter: "Queen",
-        gender: "Rock"
-    }, {
-        name: "Sappy",
-        interpreter: "Nirvana",
-        gender: "Grounge"
-    }, {
-        name: "Smeels like teen spirit",
-        interpreter: "Nirvana",
-        gender: "Grounge"
-    }, {
-        name: "The memory remains",
-        interpreter: "Metallica",
-        gender: "Trash"
-    }, {
-        name: "Stairway to heaven",
-        interpreter: "Led Zeppelin",
-        gender: "Rock"
-    }, {
-        name: "Leave that thing alone",
-        interpreter: "Rush",
-        gender: "Progressive Rock"
-    }, {
-        name: "Confortably numb",
-        interpreter: "Pink Floyd",
-        gender: "Progressive Rock"
+const createUserObject = () => {
+
+    var inputCollection = document.getElementsByTagName("input")
+    console.log(inputCollection)
+
+    var userObject = {}
+
+    for(let i = 0; i < inputCollection; i++){
+        console.log(inputCollection[i])
+        let objectKey = inputCollection[i].dataset.pointsTo
+        let objectValue = inputCollection[i].value
+        userObject[objectKey] = objectValue
+        console.log(userObject)
     }
-]
+}
 
-var elementsByTagname = document.getElementsByTagName("song-name")
-console.log(elementsByTagname)
+/*var nameInput = document.getElementById('input-name');
+nameInput.addEventListener("keyup", (event)=> {
+    let inputValue = event.target.value;
+    console.log(inputValue)
+})
 
+var adressInput = document.getElementById('input-adress');
+console.log(adressInput.value)
+
+var phoneInput = document.getElementById('input-phone');
+console.log(adressInput.value)*/
+
+var inputCollection =document.querySelectorAll('input')
+
+console.log(inputCollection)
+
+inputCollection.forEach( element => {
+    console.log(element)
+    element.addEventListener("keyup", (event) => {
+        let inputValue = event.target.value
+        console.log(inputValue)
+    })
+})
